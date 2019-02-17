8604b86279b3a6d827506acdb2d610e764e1d436
Elibay <elibay98@gmail.com>
2019-02-18 00:30:29 +0600

    Added functions of buttons and text of Quiz

--- Week 2/QuizUp/QuizUp/Controller/QuizViewController.swift
+++ Week 2/QuizUp/QuizUp/Controller/QuizViewController.swift
@@ -10,8 +10,39 @@ import UIKit
 
 class QuizViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var secondButton: UIButton!
+    @IBOutlet weak var thirdButton: UIButton!
+    @IBOutlet weak var fourthButton: UIButton!
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var textLabel: UILabel!
+    var manager = QuizManager()
+    
+    @IBAction func questionButtonTapped(_ sender: UIButton) {
+        let tag = sender.tag
+        let question = manager.getCurrentQuestion()
+        if tag == question.answer {
+            sender.backgroundColor = #colorLiteral(red: 0.1803921569, green: 0.8, blue: 0.4431372549, alpha: 1)
+        } else {
+            sender.backgroundColor = #colorLiteral(red: 0.9058823529, green: 0.2980392157, blue: 0.2352941176, alpha: 1)
+        }
+    }
+    func show() -> Void {
+        let question = manager.getCurrentQuestion()
+        textLabel.text = question.text
+        firstButton.setTitle(question.varinats[0], for: .normal)
+        secondButton.setTitle(question.varinats[1], for: .normal)
+        thirdButton.setTitle(question.varinats[2], for: .normal)
+        fourthButton.setTitle(question.varinats[3], for: .normal)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        show()
+        
     }
     
 
--- Week 2/QuizUp/QuizUp/Model/QuestionGenerator.swift
+++ Week 2/QuizUp/QuizUp/Model/QuestionGenerator.swift
@@ -14,11 +14,11 @@ class QuestionGenerator {
         
         var questions = [Question]()
         
-        questions.append(Question.init(text: "Which ocean lies on the east coast of the United States", varinats: ["Atlantic", "Eastern", "Indian", "Pacific"], answer: 0))
+        questions.append(Question.init(text: "Which ocean lies on the east coast of the United States?", varinats: ["Atlantic", "Eastern", "Indian", "Pacific"], answer: 0))
         
         questions.append(Question.init(text: "Which is the world's highest mountain?", varinats: ["Makalu", "Kilimanjaro", "K2", "Mount Everest"], answer: 3))
         
-        questions.append(Question.init(text: "How many years lived XXXTentation", varinats: ["18", "20", "30", "25"], answer: 1))
+        questions.append(Question.init(text: "How many years lived XXXTentation?", varinats: ["18", "20", "30", "25"], answer: 1))
         
         return questions
     }
